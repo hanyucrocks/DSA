a80ed9484b534b072609dc1215f3cad3ba182ce8
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-04-04 20:39:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.92%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int low = 0, high = n * m -1;
+        while(low <= high){
+            int mid = (low + high )/2, row = mid/m, col = mid % m;
+            if(matrix[row][col] == target) return 1;
+            else if(matrix[row][col] < target) low = mid+1;
+            else high = mid-1;
+        }
+        return 0;
+    }
+};
