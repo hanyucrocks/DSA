01c81323067422c6c22ed03a575e7156a6cfff64
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-05-23 22:10:48 +0530

    Time: 2070 ms (5.41%) | Memory: 7.7 MB (90.31%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        if(n<=1) return 1;
+        int count = 0;
+        for(int i = 1; i <= n; i++){
+            int left = numTrees(i-1);
+            int right = numTrees(n-i);
+            count += left * right;
+        }
+        return count;
+    }
+};
