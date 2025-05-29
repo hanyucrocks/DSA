d56a3ee4e8b8debc1c660b14295edbc5588d70bd
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-05-30 00:35:07 +0530

    Time: 0 ms (100.00%) | Memory: 74.4 MB (5.11%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -3,14 +3,12 @@ public:
     int maxSubArray(vector<int>& nums) {
         // dp se kaise kare
         int n = nums.size();
-        int maxi = INT_MIN;
-                int sum = 0;
-        for(int i = 0; i < n; i++){
-                sum += nums[i];
-                if(sum > maxi)
-                    maxi = sum;
-                if(sum < 0)
-                    sum = 0; 
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        int maxi = dp[0];
+        for(int i = 1; i < n; i++){
+            dp[i] = max(nums[i], dp[i-1] + nums[i]);
+            maxi = max(maxi, dp[i]);
         }
         return maxi;
     }
