3cd259bb2d162957e0a377f5fdf29699fa9e0038
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-05-30 12:09:01 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (85.96%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,13 +4,12 @@ public:
         int n = nums.size();
         int low = 0, high = n-1;
         int first = -1;
-        while(low<=high){
-            int mid = (low + high)/2;
+        while(low <= high){
+            int mid = (high + low) / 2;
             if(nums[mid]==target){
                 first = mid;
                 high = mid-1;
-            }
-            else if(nums[mid]<target) low = mid+1;
+            } else if(target >nums[mid]) low = mid+1;
             else high = mid-1;
         }
         return first;
@@ -19,16 +18,16 @@ public:
         int n = nums.size();
         int low = 0, high = n-1;
         int last = -1;
-        while(low<=high){
-            int mid = (low + high)/2;
-            if(nums[mid]==target){
-                last = mid;
-                low = mid+1;
-            }
-            else if(nums[mid]<target) low = mid+1;
-            else high = mid-1;
+       while(low <= high){
+        int mid = (low + high) / 2;
+        if(nums[mid] == target){
+            last = mid;
+            low=mid+1;
         }
-        return last;
+        else if(target > nums[mid]) low = mid+1;
+        else high = mid-1;
+       }
+       return last;
     }
     
     vector<int> searchRange(vector<int>& nums, int target) {
