37a4e28ffb171a8a3d5fcc7cd6a7951a1e5337ff
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-05-31 23:20:57 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (14.65%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+       unordered_map<int, int> mpp;
+       stack<int> st;
+       int n = nums2.size();
+       for(int i = n - 1; i >= 0; i--){
+        while(!st.empty() && st.top()<=nums2[i]) st.pop();
+        if(st.empty()) mpp[nums2[i]] = -1;
+        else mpp[nums2[i]] = st.top();
+        st.push(nums2[i]);
+       }
+       vector<int> ans;
+       for(int sex : nums1){
+        ans.push_back(mpp[sex]);
+       }
+       return ans;
+    }
+};
