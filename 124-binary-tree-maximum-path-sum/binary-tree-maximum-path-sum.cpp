f918239a2b1ead067d506d5deff28534dfc6a519
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-01 22:13:30 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (49.78%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -12,12 +12,12 @@
 class Solution {
 public:
     int maxPathSum(TreeNode* root) {
-        int maxi=INT_MIN;
+        int maxi = INT_MIN;
         maxPathDown(root, maxi);
         return maxi;
     }
     int maxPathDown(TreeNode* root, int &maxi){
-        if(root==NULL) return 0;
+        if(root == NULL) return 0;
         int l = max(0, maxPathDown(root->left, maxi));
         int r = max(0, maxPathDown(root->right, maxi));
         maxi = max(maxi, l + r + root->val);
