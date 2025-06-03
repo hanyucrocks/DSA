0c2fe15b8ebd44d16f320df78964fcc07c6b40cb
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-03 18:52:26 +0530

    Time: 7 ms (51.29%) | Memory: 19.2 MB (75.78%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int V = numCourses;
+        vector<int> indegree(V, 0);
+        vector<vector<int>> adj(V);
+        
+        // Build the adjacency list and compute indegrees
+        for(auto& edge : prerequisites){
+            int u = edge[0], v = edge[1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        
+        queue<int> q;
+        for(int i= 0; i < V; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front(); q.pop();
+            topo.push_back(node);
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        // reverse(topo.begin(), topo.end());
+        if(topo.size() == V) return true;
+        else return 0;
+    }
+};
