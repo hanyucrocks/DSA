477158904e8382e218930025d4e331a85857ffe3
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-03 14:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (12.63%) - LeetSync

---
+++ 290-word-pattern/word-pattern.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+        istringstream iss(s);
+        vector<string> words;
+        string word;
+        while (iss >> word) {
+            words.push_back(word);
+        }
+        if (words.size() != pattern.size())
+            return false;
+        for (int i = 0; i < pattern.size(); i++) {
+            char c = pattern[i];
+            string w = words[i];
+
+            if (charToWord.count(c)) {
+                if (charToWord[c] != w)
+                    return false;
+            } else
+                charToWord[c] = w;
+
+            if (wordToChar.count(w)) {
+                if (wordToChar[w] != c)
+                    return false;
+            } else
+                wordToChar[w] = c;
+        }
+        return true;
+    }
+};
