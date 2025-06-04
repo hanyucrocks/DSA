3c3bd226f3b6cb9e0d5d66c8db84cd5dbd0fed3d
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-04 17:01:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (45.69%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -9,7 +9,9 @@ public:
         dp[n-2] = max(nums[n-1], nums[n-2]);
 
         for(int i = n -3; i >= 0; i--){
-            dp[i] = max(nums[i] + dp[i+2], dp[i+1]);
+            int take = nums[i] + dp[i+2];
+            int nottake = dp[i+1];
+            dp[i] = max(take, nottake);
         }
         return dp[0];
     }
