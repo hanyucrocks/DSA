071834242b1ffc15235d84e3433d84e77429c6e7
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-05 19:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (49.24%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         if(strs.empty()) return "";
-        for(int i = 0; i < strs[0].length(); i++){
-            char ch = strs[0][i];
-            for(int j = 1; j < strs.size(); j++){
-                if(i >= strs[j].length() || strs[j][i] != ch){
-                    return strs[0].substr(0, i);
-                }
-            }
-        }
-        //
-        return strs[0];
+        sort(strs.begin(), strs.end());
+        string first = strs.front();
+        string last = strs.back();
+        int minlen = min(first.size(), last.size());
+        int i = 0;
+        while(i < minlen && first[i] == last[i]) i++;
+        return first.substr(0, i);
     }
 };
