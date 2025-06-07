f0be921de77dbfe468914cbce09131e516f6ba80
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-07 14:54:46 +0530

    Time: 156 ms (37.34%) | Memory: 26.1 MB (66.23%) - LeetSync

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        priority_queue<pair<char, int>, vector<pair<char, int>>,
+                       greater<pair<char, int>>>
+            pq;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '*') {
+                auto [c, i] = pq.top();
+                pq.pop();
+                s[-i] = ' ';
+            } else
+                pq.push({s[i], -i});
+        }
+        string res = "";
+        for (char c : s) {
+            if (c != ' ' && c != '*') {
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+};
