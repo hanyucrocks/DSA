b1590a821fbfebc2aca979c355b9e4626bf16773
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-09 17:31:01 +0530

    Time: 27 ms (40.41%) | Memory: 26.7 MB (71.77%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,42 +1,42 @@
 class Solution {
-int lcs(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
+public:
+    int lcs(string s1, string s2){
+        int n = s1.size();
+        int m = s2.size();
         vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
-        // bottom up - O to n - 1
-        for(int j = 0; j <= m; j++) dp[0][j] = 0;
         for(int i = 0; i <= n; i++) dp[i][0] = 0;
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++){
-                if(text1[i-1]==text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        for(int j = 0; j <= m; j++) dp[0][j] = 0;
+        for(int ind1 = 1; ind1 <= n; ind1++){
+            for(int ind2 = 1; ind2 <= m; ind2++){
+                // if same
+                if(s1[ind1-1] == s2[ind2-1]) dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                else dp[ind1][ind2] = max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
             }
         }
-    return dp[n][m];
-}
-
-public:
+        return dp[n][m];
+    }
     string shortestCommonSupersequence(string str1, string str2) {
-      int n = str1.size();
+        int n = str1.size();
         int m = str2.size();
         vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
-        // bottom up - O to n - 1
-        for(int j = 0; j <= m; j++) dp[0][j] = 0;
         for(int i = 0; i <= n; i++) dp[i][0] = 0;
-        for(int i = 1; i <= n; i++) {
+        for(int j = 0; j <= m; j++) dp[0][j] = 0;
+        for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
-                if(str1[i-1]==str2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                // if same
+                if(str1[i-1] == str2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
                 else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
         }
         string ans = "";
-        int i = n, j = m;
+        int i = n;
+        int j = m;
         while(i > 0 && j > 0){
-            if(str1[i-1]==str2[j-1]){
+            if(str1[i-1] == str2[j-1]){
                 ans += str1[i-1];
                 i--; j--;
             }
-            else if(dp[i-1][j] > dp[i][j-1]) {
+            else if(dp[i-1][j] > dp[i][j-1]){
                 ans += str1[i-1];
                 i--;
             }
@@ -45,13 +45,13 @@ public:
                 j--;
             }
         }
-        while(i > 0){
+        while(i > 0){ 
             ans += str1[i-1]; i--;
         }
         while(j > 0){
             ans += str2[j-1]; j--;
         }
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
