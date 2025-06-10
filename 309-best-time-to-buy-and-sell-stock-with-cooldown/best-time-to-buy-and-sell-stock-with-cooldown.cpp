199e20f5eb30fed143d700f255ef40104bb1aefd
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-10 14:52:31 +0530

    Time: 1 ms (47.18%) | Memory: 16.2 MB (50.23%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -12,9 +12,19 @@ public:
         return dp[ind][buy] = profit;
     }
     int maxProfit(vector<int>& prices) {
-        // buy, sell, cooldown, buy sell cooldown
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int> (2, -1));
-        return f(0, 1, prices, dp);
+        vector<vector<int>> dp(n+2, vector<int> (2, 0));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                long profit = INT_MIN;
+                if (buy) {
+                    profit = max(-prices[ind] + dp[ind+1][0], dp[ind+1][1]);
+                } else
+                    profit = max(prices[ind] + dp[ind+2][1], dp[ind+1][0]);
+                dp[ind][buy] = profit;
+            }
+        }
+        // return f(0, 1, prices, dp);
+        return dp[0][1];
     }
 };
