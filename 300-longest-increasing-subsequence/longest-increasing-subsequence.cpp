0de48e5949c09474a57f4fbb66bd2ff66c66d2c1
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-11 19:41:06 +0530

    Time: 2 ms (82.19%) | Memory: 14 MB (87.49%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    int f(int ind, int prevind, vector<int> &arr, int n, vector<vector<int>> &dp){
-        if (ind == n) return 0;
-        if(dp[ind][prevind+1]!=-1) return dp[ind][prevind+1];
-        int len = 0 + f(ind+1, prevind, arr, n ,dp);
-        // take
-        if(prevind == -1 || arr[ind] > arr[prevind]){
-            len = max(len, 1 + f(ind+1, ind, arr, n, dp));
-        }
-        return dp[ind][prevind+1] = len;
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-       vector<int> dp(n, 1);
-       int maxi = INT_MIN;
-       for(int ind = 0; ind < n; ind++){
-        for(int prev= 0; prev < ind; prev++){
-            if(nums[prev]<nums[ind]) dp[ind] = max(dp[ind], 1 + dp[prev]); 
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > temp.back()) {
+                temp.push_back(nums[i]);
+            } else {
+                auto it = lower_bound(temp.begin(), temp.end(), nums[i]);
+                *it = nums[i];
+            }
         }
-        maxi = max(maxi, dp[ind]);
-       }
-       return maxi;
+        
+        return temp.size();
     }
 };
