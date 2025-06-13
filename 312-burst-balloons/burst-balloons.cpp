5612f416b7f264cf78a032d1643d4731681e92f7
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-14 00:32:03 +0530

    Time: 87 ms (92.36%) | Memory: 13.9 MB (57.56%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -12,12 +12,22 @@ public:
     }
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-        vector<int> arr;
-        vector<vector<int>> dp(n+2, vector<int> (n+2, -1));
-        arr.push_back(1);
-        for(int x : nums) arr.push_back(x);
-        arr.push_back(1);
-        return f(1, n, arr, dp);
+        nums.push_back(1);
+        nums.insert(nums.begin(), 1);
+        vector<vector<int>> dp(n+2, vector<int> (n+2, 0));
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= n; j++){
+                if(i > j) continue;
+                int maxi = INT_MIN;
+        for(int k = i; k <= j; k++){
+            int steps = nums[i-1] * nums[k] * nums[j+1] + dp[i][k-1] + dp[k+1][j];
+            maxi = max(maxi, steps);
+        }
+        dp[i][j] = maxi;
+            }
+        }
+        // return f(1, n, arr, dp);
 
+    return dp[1][n];
     }
 };
