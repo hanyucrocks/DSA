048b66243f6c0619c820b23ff38b93a6db5944b7
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-13 23:30:49 +0530

    Time: 20 ms (70.61%) | Memory: 14.2 MB (9.81%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -18,7 +18,19 @@ public:
         cuts.insert(cuts.begin(), 0);
         sort(cuts.begin(), cuts.end());
         int c = cuts.size();
-        vector<vector<int>> dp(c, vector<int> (c, -1));
-        return f(1, c-2, cuts, dp);
+        vector<vector<int>> dp(c+2, vector<int> (c+2, 0));
+        // return f(1, c-2, cuts, dp);
+        for(int i = c- 2; i >= 1; i--){
+            for(int j = 1; j <= c- 2; j++){
+                if(i > j) continue;
+                int mini = INT_MAX;
+        for(int ind = i; ind <= j; ind++){
+            int cost = cuts[j+1] - cuts[i-1] + dp[i][ind-1] + dp[ind+1][j];
+            mini = min(mini, cost);
+        }
+        dp[i][j]=mini;
+            }
+        }
+        return dp[1][c-2];
     }
 };
