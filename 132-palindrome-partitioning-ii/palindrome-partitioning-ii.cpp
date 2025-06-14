ca9337643c154314ca3ef9a31955c0172a5fe5a4
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-14 15:01:30 +0530

    Time: 1513 ms (38.93%) | Memory: 9.3 MB (85.63%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -25,7 +25,20 @@ public:
 
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n, -1);
-        return f(0, n, s, dp);
+        // vector<int> dp(n, -1);
+        vector<int> dp(n+1, 0);
+        // return f(0, n, s, dp);
+        dp[n] = 0;
+        for(int i = n - 1; i >= 0; i--){
+            int mini = INT_MAX;
+        for(int j = i; j < n; j++){
+            if(isPalindrome(i, j, s)){
+                int cost = 1 + dp[j+1];
+                mini = min(mini, cost);
+            }
+        }
+        dp[i] = mini;
+        }
+        return dp[0] - 1;
     }
 };
