1e81b21d90801c6be59bf55e869978b0e7d9fa34
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-15 22:36:46 +0530

    Time: 65 ms (18.69%) | Memory: 87.7 MB (18.34%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> freq;
+        for(int i = 0; i < n; i++){ freq[nums[i]]++;
+        if(freq[nums[i]] > 1) return nums[i];
+        }
+        return -1;
+    }
+};
