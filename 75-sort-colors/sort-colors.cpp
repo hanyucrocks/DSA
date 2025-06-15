e4204fa7f2b1d2f7d630b1a80200a45d3d7a15b3
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-15 17:04:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.29%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,16 +2,31 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int cnt0 = 0, cnt1=0, cnt2=0;
-        for(int i = 0; i < n; i++){
-            if(nums[i]==0) cnt0++;
-            else if(nums[i]==1) cnt1++;
-            else cnt2++;
+        // int cnt0 = 0, cnt1=0, cnt2=0;
+        // for(int i = 0; i < n; i++){
+        //     if(nums[i]==0) cnt0++;
+        //     else if(nums[i]==1) cnt1++;
+        //     else cnt2++;
+        // }
+        // // int ind = 0;
+        // // now, just loop and overwrite
+        // for(int i = 0; i < cnt0; i++) nums[i] = 0;
+        // for(int i = cnt0; i < cnt0 + cnt1; i++) nums[i] = 1;
+        // for(int i = cnt0 + cnt1; i < cnt0 +cnt1 + cnt2; i++) nums[i] = 2;
+
+        // DNF Dutch National Flag
+        int low = 0, mid = 0, high = n - 1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1) mid++;
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
         }
-        int ind = 0;
-        // now, just loop and overwrite
-        for(int i = 0; i < cnt0; i++) nums[ind++] = 0;
-        for(int i = 0; i < cnt1; i++) nums[ind++] = 1;
-        for(int i = 0; i < cnt2; i++) nums[ind++] = 2;
     }
 };
