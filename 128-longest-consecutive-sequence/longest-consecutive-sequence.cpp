22ad0383eb034782c0ac5780caa29553c1118f1d
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-17 00:36:58 +0530

    Time: 83 ms (59.01%) | Memory: 89 MB (29.30%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        // sort kare toh gaye, O(NLogN) ho jayega
+        // int longest = 1;
+        // int cnt = 0;
+        // int lastSmaller = INT_MIN;
+        // sort(nums.begin(), nums.end());
+        // for(int i = 0; i < nums.size(); i++){
+        //     if(nums[i] - 1 == lastSmaller){
+        //         lastSmaller=nums[i];
+        //         cnt++;
+        //     }
+        //     else if(nums[i] != lastSmaller){
+        //         cnt = 1;
+        //         lastSmaller = nums[i];
+        //     }
+        //     longest = max(longest, cnt);
+        // }
+        // return longest;
+
+        // Optimal Solution
         int n = nums.size();
-        if(n == 0) return 0;
-        sort(nums.begin(), nums.end());
-        int longest = 1;
+        unordered_set<int> st(nums.begin(), nums.end());
         int cnt = 0;
-        int lastsmall = INT_MIN;
-        for(int i = 0; i< n; i++){
-            if(nums[i]-1==lastsmall){
-                cnt++;
-                lastsmall = nums[i];
+        int longest = 1;
+        // start looking for the first element of the sequence
+        if(n == 0) return 0;
+        for(auto it : st){
+            if(st.find(it-1) == st.end()){
+                int cnt = 1;
+                int x = it;
+                while(st.find(x+1) != st.end()){
+                    x = x+1;
+                    cnt++;
+                }
+                longest= max(longest, cnt);
             }
-            else if(nums[i] != lastsmall){
-                cnt = 1;
-                lastsmall = nums[i];
-            }
-            longest = max(longest, cnt);
         }
         return longest;
     }
