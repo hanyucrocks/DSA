d66fd92104790a3f53a64c19ad3b73b1ae0514ab
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-24 00:49:32 +0530

    Time: 354 ms (16.97%) | Memory: 291.8 MB (11.45%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
+    bool solve(int l, int r, string &s, vector<vector<int>> &dp){
+        if(l >= r) return 1;
+        if(dp[l][r]!=-1) return dp[l][r];
+        if(s[l] == s[r]) return dp[l][r] = solve(l+1, r-1, s, dp);
+        return dp[l][r] = 0;
+    }
     string longestPalindrome(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int> (n, 0));
-        if(n==0) return "";
-        int start = 0, maxLen = 1;
-        for(int i = 0; i < n; i++) dp[i][i] = 1;
-        for(int i = 0; i < n - 1; i++){
-            if(s[i] == s[i+1]){
-                dp[i][i+1]=true;
-                start = i;
-                maxLen = 2;
-            }
-        }
-        for(int i = 3; i <= n; i++){
-            for(int j = 0; j <= n - i; j++){
-                int k = j + i - 1;
-                if(s[j] == s[k] && dp[j+1][k-1]){
-                    dp[j][k] = true;
-                    start = j; maxLen = i;
+        vector<vector<int>> dp(n, vector<int> (n, -1));
+        int maxlen = 0;
+        int start = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(solve(i, j, s, dp) && (j-i+1 > maxlen)){
+                    maxlen = j - i + 1;
+                    start = i;
                 }
             }
         }
-        return s.substr(start, maxLen);
+        return s.substr(start, maxlen);
     }
 };
