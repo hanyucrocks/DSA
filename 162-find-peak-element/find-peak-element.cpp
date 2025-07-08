1991e8b7606a4bae0f879236500569f4404279db
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-07-09 01:49:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (92.00%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -2,11 +2,18 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        // intuitionn linear search
-        int ans = 0;
-        for(int i = 1; i < n; i++){
-            if((i == 0 || nums[i] > nums[i-1]) and (i == n - 1 || nums[i] > nums[i+1]) )ans = i;
+        if(n==1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+        // edge cases
+        // ab binary search
+        int low = 1, high = n - 2;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]) return mid;
+            else if(nums[mid] > nums[mid-1]) low = mid+1;
+            else high = mid-1;
         }
-        return ans;
+        return -1;
     }
 };
