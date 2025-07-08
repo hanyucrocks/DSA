b0e5c7e5e5c8fbe569a8ce641c0c132eb080c788
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-07-08 17:56:51 +0530

    Time: 12 ms (37.87%) | Memory: 26 MB (79.52%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int func(vector<int> &nums, int mid){
-       int th = 0;
-        int n = nums.size();
-        for(int i = 0; i < n; i++){
-            th += ceil((double)nums[i]/(double)mid);
+    int possible(vector<int> &nums, int mid){
+        int threshold = 0;
+        for(int i= 0; i < nums.size();i++){
+            threshold += ceil((double)nums[i] / (double)mid );
         }
-        return th;
+        return threshold;
     }
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int low =1;
-        int high = *max_element(nums.begin(), nums.end());
+        int n = nums.size();
+        int low = 1, high = *max_element(nums.begin(), nums.end());
         while(low <= high){
-            int mid = (low + high)/2;
-            int tt = func(nums, mid);
-            if( tt <= threshold) high = mid-1;
+            int mid = low + (high-low)/2;
+            if(possible(nums, mid) <= threshold) high = mid - 1;
             else low = mid+1;
         }
         return low;
