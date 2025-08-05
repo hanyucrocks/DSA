7ad2fd258f433368487c690e33482860e94670f9
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-08-05 11:15:51 +0530

    Time: 139 ms (45.81%) | Memory: 17.5 MB (54.99%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,31 +1,25 @@
 class Solution {
-    bool subsetSumToK(int n, int k, vector<int> &arr) {
-    vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
-
-    for (int i = 0; i < n; i++) 
-        dp[i][0] = true; // sum 0 is always possible
-
-    if (arr[0] <= k) 
-        dp[0][arr[0]] = true;
-
-    for (int i = 1; i < n; i++) {
-        for (int target = 1; target <= k; target++) {
-            bool notTake = dp[i - 1][target];
-            bool take = false;
-            if (arr[i] <= target) 
-                take = dp[i - 1][target - arr[i]];
-            dp[i][target] = take || notTake;
-        }
-    }
-
-    return dp[n - 1][k];
-}
 public:
     bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        // target == sum / 2;
         int sum = accumulate(nums.begin(), nums.end(), 0);
-       int target = sum / 2;
-       if(sum % 2 != 0) return false;
-       if(subsetSumToK(nums.size(), target, nums)) return 1;
-       return 0;
+        if(sum % 2 != 0) return 0;
+        int k = sum/2;
+        vector<vector<bool>> dp(n, vector<bool> (k+1, 0));
+        // dp[i][sum]
+        for(int i = 0; i < n; i++)
+            dp[i][0] = 1;
+        if(nums[0] <= k)
+            dp[0][nums[0]] = 1;
+        for(int i = 1; i < n; i++){
+            for(int target = 1; target <= k; target++){
+                bool nottake = dp[i-1][target];
+                bool take = false;
+                if(nums[i] <= target) take = dp[i-1][target - nums[i]];
+                dp[i][target] = take || nottake;
+            }
+        }
+        return dp[n-1][k];
     }
 };
