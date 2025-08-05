636a6e567800dd9004d0fc944a161fd5cffe6233
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-08-05 16:52:28 +0530

    Time: 43 ms (11.72%) | Memory: 39 MB (22.62%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size();        // rows
-        int n = mat[0].size();     // cols
-
+        int m = mat.size();
+        int n = mat[0].size();
         queue<pair<pair<int, int>, int>> q;
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        vector<vector<int>> dist(m, vector<int>(n, 0));
-
-        // Push all zero cells initially into the queue
+        vector<vector<int>> vis(m, vector<int> (n, 0));
+        vector<vector<int>> dist(m, vector<int> (n, 0));
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(mat[i][j] == 0){
@@ -17,9 +14,7 @@ public:
                 }
             }
         }
-
-        vector<pair<int, int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-
+        vector<pair<int, int>> directions = {{-1, 0}, {0, -1}, {+1, 0}, {0, +1}};
         while(!q.empty()){
             int row = q.front().first.first;
             int col = q.front().first.second;
@@ -31,10 +26,9 @@ public:
             for(auto [dx, dy] : directions){
                 int nr = row + dx;
                 int nc = col + dy;
-
-                if(nr >= 0 && nr < m && nc >= 0 && nc < n && vis[nr][nc] == 0){
-                    vis[nr][nc] = 1;   
-                    q.push({{nr, nc}, steps + 1});
+                if(nr >=0 and nr < m and nc >= 0 and nc < n and vis[nr][nc] == 0){
+                    vis[nr][nc] = 1;
+                    q.push({{nr, nc}, steps+1});
                 }
             }
         }
