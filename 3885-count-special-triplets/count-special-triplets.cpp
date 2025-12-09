a83615387a369d12972a1b31780093aef0a611bf
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2025-12-09 23:57:33 +0530

    Time: 623 ms (67.52%) | Memory: 314.4 MB (89.05%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -4,7 +4,9 @@ public:
     int specialTriplets(vector<int>& nums) {
         int n = nums.size();
         long long ans = 0;
-        unordered_map<long long, long long> freqprev, freqnext;
+        int mx = *max_element(nums.begin(), nums.end());
+        int size = 2*mx+5;
+        vector<int> freqnext(size, 0), freqprev(size, 0);
         for(int x : nums){
             freqnext[x]++;
         }
@@ -12,10 +14,12 @@ public:
             long long cur = nums[j];
             freqnext[cur]--; // next cannot be used now
             long long target = cur * 2;
+            if(target < size){
             long long left = 0, right =0;
-            if(freqprev.count(target)) left = freqprev[target];
-            if(freqnext.count(target)) right = freqnext[target];
+             left = freqprev[target];
+            right = freqnext[target];
             ans += (left % mod) * (right % mod) % mod;
+            }
             freqprev[cur]++;
         }
         return (int)(ans % mod);
