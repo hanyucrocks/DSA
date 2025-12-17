2a7db627b169291dafc59dbb73afa4eff87c5e45
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2025-12-17 23:41:08 +0530

    Time: 7 ms (15.52%) | Memory: 22.1 MB (12.84%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    int f(int ind, int buy, vector<int> &prices, vector<vector<int>> &dp){
+    int f(int idx, int buy, vector<int>&prices, vector<vector<int>> &dp){
         int n = prices.size();
-        if(ind == n) return 0;
-        if(dp[ind][buy] != -1) return dp[ind][buy];
-        int profit = INT_MIN;
+        if(idx == n) return 0;
+        if(dp[idx][buy] != -1) return dp[idx][buy];
+        // buy can be 0 or 1
+        int profit = -1e9;
         if(buy){
-            profit = max(-prices[ind] + f(ind+1, 0, prices, dp), 0 + f(ind+1, 1, prices, dp));
+            profit = max(-prices[idx] + f(idx+1, 0, prices, dp), f(idx+1, 1, prices, dp));
         }
-        else profit = max(prices[ind] + f(ind+1, 1, prices, dp), f(ind+1, 0, prices, dp));
-        return dp[ind][buy] = profit;
+        else // yaha pe sell hoga
+        {
+            profit = max(prices[idx] + f(idx+1, 1, prices, dp), f(idx+1, 0, prices, dp));
+        }
+        return dp[idx][buy] = profit;
+
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<long> ahead(2, 0), cur(2, 0);
-        ahead[0] = ahead[1] = 0;
-        for(int ind = n - 1; ind >= 0; ind--){
-            for(int buy =0; buy <=1; buy++){
-                long profit = INT_MIN;
-        if(buy){
-            profit = max(-prices[ind] + ahead[0], 0 + ahead[1]);
-        }
-        else profit = max(prices[ind] + ahead[1], ahead[0]);
-        cur[buy] = profit;
-            }
-            ahead = cur;
-        }
-        // return f(0, 1, prices, dp);
-        return ahead[1];
+        vector<vector<int>> dp(n, vector<int> (2, -1));
+        return f(0, 1, prices, dp);
     }
 };
