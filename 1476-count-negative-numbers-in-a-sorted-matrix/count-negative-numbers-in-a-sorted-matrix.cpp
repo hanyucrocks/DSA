1d64246675b494b4dc6a85988b0da45743dc1d92
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2025-12-28 08:49:06 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (35.40%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -3,12 +3,14 @@ public:
     int countNegatives(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int cnt = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] < 0) cnt++;
+        int r = 0, c = n -1, ans=0;
+        while(r < m and c>=0){
+            if(grid[r][c] < 0){
+                ans += (m - r);
+                c--;
             }
+            else r++;
         }
-        return cnt;
+        return ans;
     }
 };
