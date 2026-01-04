bc366b19381f15dd32d7a594d6a22826e71ec268
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-01-04 19:47:56 +0530

    Time: 75 ms (7.17%) | Memory: 78.2 MB (15.64%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -5,12 +5,20 @@ public:
         int n = nums.size();
         int k = 1;
         // the answer lies between 1 to n.
-        sort(nums.begin(), nums.end());
-        for(int num : nums){
-            if(num < k) continue;
-            if(num == k) k = k + 1;
-            else return k;
+        // sort(nums.begin(), nums.end());
+        // for(int num : nums){
+        //     if(num < k) continue;
+        //     if(num == k) k = k + 1;
+        //     else return k;
+        // }
+        // return k;
+        unordered_set<int> hash;
+        for(int x : nums){
+            if(x > 0)hash.insert(x);
         }
-        return k;
+        while(true){
+            if(hash.find(k)==hash.end()) return k;
+            k =k+1;
+        }
     }
 };
