14817623885bb72ee0b8c8b94b0552093d4f8dc0
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-01-10 18:42:38 +0530

    Time: 14 ms (78.67%) | Memory: 18.3 MB (49.34%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,32 +1,54 @@
 class Solution {
 public:
-    int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
-        int n = s1.size();
-        int m = s2.size();
-        if(i == n){
-            int sum = 0;
-            for(int k = j; k < m; k++){
-                sum += s2[k];
-            }
-            return sum;
-        }
-        if(j == m){
-            int sum = 0;
-            for(int k = i; k < n; k++){
-                sum += s1[k];
-            }
-            return sum;
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s1[i] == s2[j]) return dp[i][j] = f(i+1,  j+1, s1, s2, dp);
-        else{
-            return dp[i][j] = min(s1[i] + f(i+1, j, s1, s2, dp), s2[j] + f(i, j+1, s1, s2, dp));
-        }
-    }
+    // int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
+    //     int n = s1.size();
+    //     int m = s2.size();
+    //     if(i == n){
+    //         int sum = 0;
+    //         for(int k = j; k < m; k++){
+    //             sum += s2[k];
+    //         }
+    //         return sum;
+    //     }
+    //     if(j == m){
+    //         int sum = 0;
+    //         for(int k = i; k < n; k++){
+    //             sum += s1[k];
+    //         }
+    //         return sum;
+    //     }
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     if(s1[i] == s2[j]) return dp[i][j] = f(i+1,  j+1, s1, s2, dp);
+    //     else{
+    //         returndp[i][j] = min(s1[i] + f(i+1, j, s1, s2, dp), s2[j] + f(i, j+1, s1, s2, dp));
+    //     }
+    // }
+    // int ASCII(const string &s){
+    //     int sum = 0;
+    //     for(char c : s) sum += c;
+    //     return sum;
+    // }
     int minimumDeleteSum(string s1, string s2) {
         int n = s1.size();
         int m = s2.size();
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return f(0, 0, s1, s2, dp);
+        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+        dp[n][m] = 0;
+        // s1 exhaused so s2 is iin work
+        for(int j = m -1; j >= 0; j--){
+            dp[n][j] = dp[n][j+1] + (s2[j]);
+        }
+        // s2 exhaused
+        for(int i = n - 1; i >= 0; i--){
+            dp[i][m] = dp[i+1][m] + (s1[i]);
+        }
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = m - 1; j >= 0; j--){
+                if(s1[i] == s2[j]) dp[i][j] = dp[i+1][j+1];
+                else{
+                    dp[i][j] = min((s1[i]) + dp[i+1][j], (s2[j]) + dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
