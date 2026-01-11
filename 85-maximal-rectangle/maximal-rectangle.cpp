6fe9025fc711f4d57dcb44b15e0b76330f10bb23
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-01-11 10:38:59 +0530

    Time: 1 ms (90.35%) | Memory: 18 MB (66.56%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,42 +1,63 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
+    // int f(int i, int j, vector<vector<char>> &matrix, vector<vector<int>> &dp){
+    //     int n = matrix.size();
+    //     int m = matrix[0].size();
+    //     if(i < 0 or j < 0) return 0;
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     // left or right directions? dp kaaaam NAHI KAREGA IDHARRRRRRRRRRRRRRRRRRR   
+    // }
+    int largestRectangleArea(vector<int> &heights){
+        int n = heights.size();
         stack<int> st;
-        int maxArea=0;
-        for(int i = 0; i <= heights.size() -1; i++){
-            while(!st.empty() && heights[st.top()] >= heights[i]){
-                int element = st.top();
+        int largestarea = INT_MIN;
+        int area, nse, pse;
+        // traverse the array
+        for(int i = 0;i < n; i++){
+            // work on the stack
+            while(!st.empty() and heights[st.top()] >= heights[i]){
+                // get the top and pop it
+                int ind = st.top();
                 st.pop();
-                int nse = i;
-                int pse = st.empty() ? -1 : st.top();
-                maxArea = max(heights[element] * (nse - pse - 1), maxArea);
+                // nse and pse stuff
+                pse = st.empty() ? -1 : st.top();
+                // nse is the index
+                nse = i;
+                area = heights[ind] * (nse-pse - 1);
+                largestarea = max(largestarea, area);
             }
             st.push(i);
         }
+        // for elements that are not popped from the stack.
         while(!st.empty()){
-            int nse = heights.size();
-            int element = st.top();
+            nse = n;
+            int ind = st.top();
             st.pop();
-            int pse = st.empty() ? -1 : st.top();
-            maxArea = max(heights[element] * (nse - pse - 1), maxArea);
+            pse = st.empty() ? -1 : st.top();
+            area = heights[ind] * (nse-pse - 1);
+                largestarea = max(largestarea, area);
         }
-        return maxArea;
+        return largestarea;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
+        // n rows and m cols
         int n = matrix.size();
         int m = matrix[0].size();
-        // take the histogram approach
-        // row by row niche move karenge
-        int maxA = 0;
-            vector<int> height(m, 0);
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(matrix[i][j]=='1') height[j]++;
-                else height[j] = 0;
-            }
-            int area = largestRectangleArea(height);
-            maxA = max(maxA, area);
+        int ans = 0;
+        // this is a problem of monotonic stack!!!!
+        /*
+        per row just use the maximum area concept of histogram....
+        prefix sum to use the 2d matrix. use row wize
+        */
+       vector<int> heights(m, 0);
+        // height is done,. now what, prefix sum
+       for(int i = 0; i < n; i++){
+        for(int j  = 0; j < m; j++){
+            if(matrix[i][j] == '1') heights[j]++;
+            else heights[j] = 0;
         }
-        return maxA;
+        ans = max(ans, largestRectangleArea(heights));
+       }
+        return ans;
     }
 };
