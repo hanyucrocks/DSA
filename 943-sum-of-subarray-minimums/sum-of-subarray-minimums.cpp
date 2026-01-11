f2194281f0d51a0e22637b7895807318e8443ab8
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-01-11 14:44:15 +0530

    Time: 17 ms (49.39%) | Memory: 47.3 MB (37.01%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& arr) {
-    int n = arr.size();
-        vector<int> nse = nextSmallerElement(arr);
-        vector<int> pse = prevSmallerElement(arr);
-        const int mod = 1e9+7;
-        long long total = 0;
-    for(int i = 0; i < n; i++){
-        int left = i - pse[i];
-        int right = nse[i] - i;
-        total = (total + (right * left * 1LL * arr[i]) % mod) % mod;
-    }
-    return total;
-    }
-    vector<int> nextSmallerElement(vector<int> &nums){
-        int n = nums.size();
+    typedef long long ll;
+    vector<int> findNSE(vector<int> &arr){
+        int n = arr.size();
+        vector<int> nse(n);
         stack<int> st;
-        vector<int> nse(n, n);
-        for(int i = n - 1; i >= 0; i--){
-            while(!st.empty() && nums[st.top()] >= nums[i]) st.pop();
+        for(int i = n - 1; i>=0;i--){
+            while(!st.empty() and arr[st.top()] >= arr[i]) st.pop();
             nse[i] = st.empty() ? n : st.top();
             st.push(i);
         }
         return nse;
     }
-    vector<int> prevSmallerElement(vector<int> &nums){
-        int n = nums.size();
-        vector<int> pse(n, -1);
+    vector<int> findPSEE(vector<int> &arr){
+        int n = arr.size();
+        vector<int> psee(n);
         stack<int> st;
         for(int i = 0; i < n; i++){
-            while(!st.empty() && nums[st.top()] > nums[i]) st.pop();
-            pse[i] = st.empty() ? -1 : st.top();
+            while(!st.empty() and arr[st.top()] > arr[i]) st.pop();
+            psee[i] = st.empty() ? -1 : st.top();
             st.push(i);
         }
-        return pse;
+        return psee;
     }
-};
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        const int mod = 1e9+7;
+        // how many subarrays can be formed. n * (n+1)/2;
+        // stack<int> st;
+        // apparently this question is faltu ka hard......
+        // prev[i] * next[i] * nums[i] = x, sum of all x is the answer.
+       // nse * psee * nums[i] combo 
+       vector<int> nse = findNSE(arr);
+       vector<int> psee = findPSEE(arr);    
+       ll total = 0;
+       for(ll i = 0; i < n; i++){
+        ll left = i - psee[i];
+        ll right = nse[i] - i;
+        total = (total + (right * left % mod) * arr[i]) % mod;
+       }
+       return total;
+    }
+}; 
