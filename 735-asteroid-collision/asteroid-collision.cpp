64e7be73571de5fdd8f3209501a93c885afb7c22
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-01-11 12:11:53 +0530

    Time: 7 ms (5.81%) | Memory: 23.3 MB (6.69%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        list<int> st;
         int n = asteroids.size();
-        for(int i = 0; i < n; i++){
-            if(asteroids[i] > 0) st.push_back(asteroids[i]);
+        list<int> st;
+        for(int i = 0; i< n; i++){
+            if(asteroids[i] > 0) st.push_back(asteroids[i]); // positives saare andar
+            // lets handle negatives now
             else{
-            while(!st.empty() && st.back() > 0 && st.back() < abs(asteroids[i])) st.pop_back();
-            if(!st.empty() && st.back() == abs(asteroids[i])) st.pop_back();
-            else if(st.empty() or st.back() < 0) st.push_back(asteroids[i]);
+                while(!st.empty() and st.back() > 0 and st.back() < abs(asteroids[i])) st.pop_back(); // smaller wala asteroid gaya
+                if(!st.empty() and st.back() == abs(asteroids[i])) st.pop_back();
+                else if(st.empty() or st.back() < abs(asteroids[i])) st.push_back(asteroids[i]);
             }
         }
-        return vector<int>(st.begin(), st.end());
+        // positive goes right negative goes left   
+        return vector<int> (st.begin(), st.end());
     }
 };
