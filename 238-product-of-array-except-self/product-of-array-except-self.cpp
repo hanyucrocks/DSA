ef7ab603469a73ec340ea1d94cd17c0c33282bc0
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-01-18 08:14:10 +0000

    Time: 0 ms (100.00%) | Memory: 40.2 MB (58.17%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,19 +4,15 @@ public:
         int n = nums.size();
         // prefix - 24 12 4 1, sufffix - 1 1 2 6
         // max 24 12 8 6
-        vector<int> pref(n), suff(n);
-        pref[0] = 1;
-        suff[n-1] = 1;
-        for(int i = 1; i < n; i++){
-            pref[i] = pref[i-1] * nums[i-1];
-        }
-        // pref 1 1 2 6
-        for(int i = n - 2; i>=0; i--){
-            suff[i] = suff[i+1] * nums[i+1];
-        }
         vector<int> ans(n);
-        for(int i = 0; i < n; i++){
-            ans[i] = pref[i] * suff[i];
+        ans[0] = 1;
+        for(int i = 1; i < n; i++){
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+        int rightprod = 1;
+        for(int i = n - 1; i >=0 ; i--){
+            ans[i] *= rightprod;
+            rightprod *= nums[i];
         }
         return ans;
     }
