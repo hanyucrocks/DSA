a25173c211295d361944d8779a0fd7e9c2aff2f9
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-01-18 06:36:56 +0000

    Time: 18 ms (84.86%) | Memory: 60.3 MB (82.91%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        // sort kare toh gaye, O(NLogN) ho jayega
-        // int longest = 1;
-        // int cnt = 0;
-        // int lastSmaller = INT_MIN;
-        // sort(nums.begin(), nums.end());
-        // for(int i = 0; i < nums.size(); i++){
-        //     if(nums[i] - 1 == lastSmaller){
-        //         lastSmaller=nums[i];
-        //         cnt++;
-        //     }
-        //     else if(nums[i] != lastSmaller){
-        //         cnt = 1;
-        //         lastSmaller = nums[i];
-        //     }
-        //     longest = max(longest, cnt);
-        // }
-        // return longest;
-
-        // Optimal Solution
         int n = nums.size();
-        unordered_set<int> st(nums.begin(), nums.end());
-        int cnt = 0;
-        int longest = 1;
-        // start looking for the first element of the sequence
         if(n == 0) return 0;
-        for(auto it : st){
-            if(st.find(it-1) == st.end()){
-                int cnt = 1;
-                int x = it;
-                while(st.find(x+1) != st.end()){
-                    x = x+1;
-                    cnt++;
-                }
-                longest= max(longest, cnt);
+        int maxi = INT_MIN;
+        int ans = 1;
+        sort(nums.begin(), nums.end());
+        // 0 0 1 2 3 4 5 6 7 8 
+        int current = 1;
+        for(int i = 0; i < n - 1; i++){
+            if(nums[i+1] - nums[i] ==1){
+                current++;
+                ans = max(ans, current);
+            }
+            if(nums[i+1] - nums[i] > 1){
+                current = 1;
+                ans = max(ans, current);
             }
         }
-        return longest;
+        return ans;
     }
 };
