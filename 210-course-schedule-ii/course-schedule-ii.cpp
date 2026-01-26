f1a9d04839088ec87ba1f274b4a1d3432c93b872
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-01-26 09:36:38 +0000

    Time: 0 ms (100.00%) | Memory: 18.2 MB (71.50%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -2,31 +2,41 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         int V = numCourses;
-        vector<int> indegree(V, 0);
         vector<vector<int>> adj(V);
-        
-        // Build the adjacency list and compute indegrees
-        for(auto& edge : prerequisites){
-            int u = edge[0], v = edge[1];
+        for(auto &edge : prerequisites){
+            int u = edge[0];
+            int v = edge[1];
             adj[u].push_back(v);
-            indegree[v]++;
         }
+        // add a check that if uv and vu exist then output nothing
         
+        // adj list done
+        vector<int> list;
         queue<int> q;
-        for(int i= 0; i < V; i++){
+        // indegree
+        vector<int> indegree(V, 0);
+        for(int i = 0;i<V;i++){
+            for(auto it : adj[i]) indegree[it]++;
+        }
+        for(int i = 0; i <V;i++){
             if(indegree[i] == 0) q.push(i);
         }
-        vector<int> topo;
         while(!q.empty()){
-            int node = q.front(); q.pop();
-            topo.push_back(node);
-            for(auto it : adj[node]){
+            int top = q.front();
+            q.pop();
+            list.push_back(top);
+            for(auto it : adj[top]){
                 indegree[it]--;
-                if(indegree[it] == 0) q.push(it);
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
             }
         }
-        reverse(topo.begin(), topo.end());
-        if(topo.size() == V) return topo;
-        else return {};
+        vector<int> ans = list;
+        reverse(ans.begin(), ans.end());
+        if((int)ans.size() == numCourses){
+            return ans;
+        }
+        return {};
     }
 };
