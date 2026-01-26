4a8bb78ce5d90dd6e626144701ef3b55b786a271
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-01-26 09:28:58 +0000

    Time: 3 ms (77.77%) | Memory: 19.4 MB (56.52%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int V = numCourses;
-        vector<int> indegree(V, 0);
+    bool kahns(int V, vector<vector<int>> &edges){
         vector<vector<int>> adj(V);
-        
-        // Build the adjacency list and compute indegrees
-        for(auto& edge : prerequisites){
-            int u = edge[0], v = edge[1];
+        for(auto &edge : edges){
+            int u = edge[0];
+            int v = edge[1];
             adj[u].push_back(v);
-            indegree[v]++;
         }
-        
+        // adj list done
+        vector<int> list;
         queue<int> q;
-        for(int i= 0; i < V; i++){
+        // indegree
+        vector<int> indegree(V, 0);
+        for(int i = 0;i<V;i++){
+            for(auto it : adj[i]) indegree[it]++;
+        }
+        for(int i = 0; i <V;i++){
             if(indegree[i] == 0) q.push(i);
         }
-        vector<int> topo;
         while(!q.empty()){
-            int node = q.front(); q.pop();
-            topo.push_back(node);
-            for(auto it : adj[node]){
+            int top = q.front();
+            q.pop();
+            list.push_back(top);
+            for(auto it : adj[top]){
                 indegree[it]--;
-                if(indegree[it] == 0) q.push(it);
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
             }
         }
-        // reverse(topo.begin(), topo.end());
-        if(topo.size() == V) return true;
-        else return 0;
+        return list.size() == V;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        return kahns(numCourses, prerequisites);
     }
 };
