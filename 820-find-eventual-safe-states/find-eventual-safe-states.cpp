09fc44c3c2efeb5ce04ae86044a87d92a1ae987c
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-01-26 10:10:16 +0000

    Time: 70 ms (8.25%) | Memory: 66.1 MB (49.72%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V = graph.size();
-        // jo touch nahi hua usko toh output karna hi hao
-        // how do i build the adj list
-        // you dont need the build the adj list here....
-        queue<int> q;
-        vector<int> ans;
-        vector<int> outdegree(V, 0);
+        int V= graph.size();
+        vector<int>indegree(V, 0);
+        vector<vector<int>> adjRev(V);
         for(int i = 0; i < V; i++){
-            outdegree[i] = graph[i].size();
-        }
-        // outgrtee
-        vector<vector<int>> rev(V);
-        for(int i = 0; i <V;i++){
-            for(int nei : graph[i]){
-                rev[nei].push_back(i);
+            for(int v : graph[i]){
+                adjRev[v].push_back(i);
+                indegree[i]++;
             }
         }
-        // reversed the graph
-        for(int i = 0; i < V; i++){
-            if(outdegree[i] == 0) q.push(i);
+        queue<int> q;
+        vector<int> list;
+        for(int i = 0; i <V;i++){
+            if(indegree[i] == 0) q.push(i);
         }
-        vector<int> safe(V, 0);
         while(!q.empty()){
             int top = q.front();
             q.pop();
-            safe[top] = 1;
-            for(int parent : rev[top]){
-                outdegree[parent]--;
-                if(outdegree[parent] == 0) q.push(parent);
+            list.push_back(top);
+            for(auto it : adjRev[top]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
             }
         }
-        for(int i = 0; i <V;i++){
-            if(safe[i]) ans.push_back(i);
-        }
-        return ans;
-        // i hope this is the adj
-
+        sort(list.begin(), list.end());
+        return list;
+        // compute indegree
     }
 };
