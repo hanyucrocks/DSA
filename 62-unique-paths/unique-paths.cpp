5b9de9f6a761101a4dcd77616a106a836859eebe
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-02-11 11:50:58 +0000

    Time: 0 ms (100.00%) | Memory: 9.5 MB (28.96%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int f(int i, int j, int m, int n, vector<vector<int>> &dp){
         if(i < 0 or j < 0) return 0;
-        dp[0][0] = 1;
+        if(i==0 and j ==0) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
         //m-1, n-1 se 0 0 aana hai, toh ulta
         int up = f(i-1, j, m, n, dp);
