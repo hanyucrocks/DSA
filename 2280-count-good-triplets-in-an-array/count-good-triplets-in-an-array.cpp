c5ab5a1c2e386e57997fcd3f65c8393461cf4f6a
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-02-19 20:04:52 +0000

    Time: 147 ms (67.31%) | Memory: 120.3 MB (34.30%) - LeetSync

--- 2280-count-good-triplets-in-an-array/count-good-triplets-in-an-array.cpp
+++ 2280-count-good-triplets-in-an-array/count-good-triplets-in-an-array.cpp
@@ -1,16 +1,16 @@
 class Solution {
     vector<long long> segTree;
     int n;
-    void buildTree(int i, int l, int r, vector<int> &arr){
-        if(l==r){
-            segTree[i] = arr[l];
-            return;
-        }
-        int mid = (l+r)/2;
-        buildTree(2*i+1, l, mid, arr);
-        buildTree(2*i+2, mid+1, r, arr);
-        segTree[i] = segTree[2*i+1] + segTree[2*i+2];
-    }
+    // void buildTree(int i, int l, int r, vector<int> &arr){
+    //     if(l==r){
+    //         segTree[i] = arr[l];
+    //         return;
+    //     }
+    //     int mid = (l+r)/2;
+    //     buildTree(2*i+1, l, mid, arr);
+    //     buildTree(2*i+2, mid+1, r, arr);
+    //     segTree[i] = segTree[2*i+1] + segTree[2*i+2];
+    // }
     int query(int start, int end, int i, int l, int r){
         if(l > end or r < start) return 0;
         if(l >= start and r <= end) return segTree[i];
@@ -47,7 +47,7 @@ public:
         updateST(0, 0, n - 1, m[nums1[0]]); // ye update kardega segtree ko
         for(int i = 1; i < n; i++){
             int idx = m[nums1[i]]; // idx in nums2
-            long long leftCommonCount = query(0, idx - 1, 0, 0, n - 1);
+            long long leftCommonCount = query(0, idx, 0, 0, n - 1);
             long long leftUnCommonCount = i - leftCommonCount;
             long long elementsAfterIdxNums2 = (n - 1 -idx);
             long long rightCommonCount = elementsAfterIdxNums2-leftUnCommonCount;
