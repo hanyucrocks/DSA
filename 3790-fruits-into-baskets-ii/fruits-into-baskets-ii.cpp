2c42208132bb08354a7a9e0aa049a42ecec8e6d5
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-02-19 20:10:10 +0000

    Time: 0 ms (100.00%) | Memory: 48.7 MB (93.37%) - LeetSync

---
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size();
+        // st approach...
+        // chota constraint....
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            bool placed = false;
+            for(int j = 0; j < n; j++){
+                if(baskets[j] >= fruits[i]){
+                    baskets[j] = -1;
+                    placed = true;
+                    break;
+                }
+            }
+            if(!placed) ans++;
+        }
+        return ans;
+    }
+};
