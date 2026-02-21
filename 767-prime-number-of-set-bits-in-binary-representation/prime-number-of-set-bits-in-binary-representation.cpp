cc16367d6d18d391eef094c680e0e39160f432e9
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-02-21 18:07:16 +0000

    Time: 0 ms (100.00%) | Memory: 8 MB (35.12%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,9 +1,7 @@
 class Solution {
     bool isPrime(int n){
         if(n == 1) return false;
-        if(n ==2) return true;
-        if(n % 2 ==0) return false;
-            for(int i = 3; i <= sqrt(n); i+=2){
+            for(int i = 2; i * i <= n; i++){
                 if(n % i ==0) return false;
             }
         return true;
