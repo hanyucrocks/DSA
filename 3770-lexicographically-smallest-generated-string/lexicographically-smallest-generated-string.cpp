b2d5d19a57359f18e55c2af204635aba1bf682b3
Agnish Bauri <146408033+hanyucrocks@users.noreply.github.com>
2026-03-31 23:58:50 +0530

    Time: 13 ms (84.75%) | Memory: 11.2 MB (45.76%) - LeetSync

--- 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.cpp
+++ 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    bool isSame(string &word, string &str2, int i , int m){
+    bool isSame(string &word, string &str2, int i, int m){
         for(int j = 0; j < m; j++){
             if(word[i] != str2[j]) return false;
             i++;
@@ -10,42 +10,41 @@ public:
     string generateString(string str1, string str2) {
         int n = str1.size();
         int m = str2.size();
-        // jaha T hai waha pe str2 fill karo
         int N = n + m - 1;
-        string word(N, '%');
-        // T fill them with str2
-        vector<bool> change(N, false);
+        string word(N, '$');
+        vector<bool> canChange(N, false);
+        // process the Ts
         for(int i = 0; i < n; i++){
             if(str1[i] == 'T'){
                 int i_ = i;
-                // fill with str2
                 for(int j = 0; j < m; j++){
-                    if(word[i_] != '%' and word[i_] != str2[j]) return "";
+                    if(word[i_] != '$' and word[i_] != str2[j]) return "";
                     word[i_] = str2[j];
                     i_++;
                 }
             }
         }
+        // now next
         for(int i = 0; i < N; i++){
-            if(word[i] == '%'){
+            // the empty spaces
+            if(word[i] == '$'){
                 word[i] = 'a';
-                change[i] = 1;
+                canChange[i] = true;
             }
         }
-        // process the F
+        // now F
         for(int i = 0; i < n; i++){
             if(str1[i] == 'F'){
                 if(isSame(word, str2, i, m)){
-                    // agar same nikal gaya toh break karo
                     bool changed = false;
                     for(int k = i + m - 1; k >= i; k--){
-                        if(change[k] == 1){
+                        if(canChange[k] == true){
                             word[k] = 'b';
                             changed = true;
                             break;
                         }
                     }
-                    if(changed == false) return ""; // change nahi kar paya
+                    if(changed == false) return "";
                 }
             }
         }
